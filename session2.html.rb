e54bef98e771567c39b7f4a62285571915aa42a5
里千晴 <satochiharu@satonoMacBook-Pro.local>
2020-06-05 13:37:51 +0900

    [update]session2 学習終了

--- session2.html.rb
+++ session2.html.rb
@@ -14,4 +14,43 @@ def add(a, b)
   a + b
 end
 
-# puts add(1,2)
+# case文を使ったサンプル
+country = 'japan'
+
+message =
+case country
+when 'japan'
+  'こんにちは'
+when 'us'
+  'Hello'
+when 'italy'
+  'ciao'
+else
+  '???'
+end
+
+# puts message
+
+# 三項演算子
+n = 11
+# if n > 10
+#   '10より大きい'
+# else
+#   '10以下'
+# end
+
+imo = n > 10 ? '10より大きい' : '10以下'
+puts imo
+
+# ?で終わるメソッド
+# から文字かどうか
+''.empty?
+'abc'.empty?
+# 引数の文字列が含まれているか
+'watch'.include?('wat')
+'watch'.include?('in')
+
+# エイリアスメソッド
+# どちらも文字数を返す
+'hello'.length
+'hello'.size
